b1cced672ad0192685bf5decae89a895fe39a1ae
jaganisushil <jaganisushil1836@gmail.com>
2023-11-30 10:55:32 +0530

    [ADS]-map

--- lib/constant/app_images.dart
+++ lib/constant/app_images.dart
@@ -4,4 +4,5 @@ class AppImages{
   static const String delivary = "assets/dilivary.png";
   static const String profile = "assets/profile.png";
   static const String userpic= "assets/user_photo.png";
+  static const String map= "assets/map.png";
 }
--- lib/pages/map/ridebooking.dart
+++ lib/pages/map/ridebooking.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rapido/constant/app_images.dart';
 
 class RideBooking extends StatefulWidget {
   const RideBooking({super.key});
@@ -14,9 +15,25 @@ class _RideBookingState extends State<RideBooking> {
       appBar: AppBar(
         elevation: 2,
         backgroundColor: Colors.white,
-        leading: Icon(Icons.list,color: Colors.black,),
-        shape: RoundedRectangleBorder(
+        leading: const Icon(
+          Icons.arrow_back_ios_new,
+          color: Colors.black,
+        ),
+        title: Text("Enter Destination", style: TextStyle(color: Colors.black)),
+        shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                  height: 280,
+                  width: 390,
+                  child: Image.asset(AppImages.map, fit: BoxFit.cover)),
+            ],
+          ),
+        ],
       ),
     );
   }
