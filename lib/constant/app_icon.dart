b23843a21acaacfe150d7e7ed9981b2a078203a6
priyam2531 <priyameruliya2531@gmail.com>
2023-12-15 10:51:53 +0530

    [PMC]-priya Ridedetail4

--- lib/constant/app_icon.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-class AppIcon {
-  static Icon menu =  Icon(Icons.menu);
-  static Icon minus = Icon(CupertinoIcons.minus);
-}
